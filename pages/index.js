74b819387970a53cd97cae66f466ee2b744848a6
Joschka Eickhoff <jeickhoff@posteo.de>
2022-11-23 15:57:04 +0100

    basic styling

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,40 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
+import styled from 'styled-components';
+
+const StyledMain = styled.main`
+    width: 100vw;
+    height: 100vh;
+    display: grid;
+    justify-content: center;
+    align-content: center;
+`;
+
+const StyledUl = styled.ul`
+`;
+
+const StyledLi = styled.li`
+    text-align: center;
+    list-style: none;
+    margin: 50px 0; 
+`;
 
 export default function Home() {
     return (
         <>
-            <ul>
-                <li>
-                    <Link href="/about">about</Link>
-                </li>
-                <li>
-                    <Link href="/projects">projects</Link>
-                </li>
-                <li>
-                    <Link href="/blog">blog</Link>
-                </li>
-            </ul>
+            <StyledMain>
+                <StyledUl>
+                    <StyledLi>
+                        <Link href="/about">about</Link>
+                    </StyledLi>
+                    <StyledLi>
+                        <Link href="/projects">projects</Link>
+                    </StyledLi>
+                    <StyledLi>
+                        <Link href="/blog">blog</Link>
+                    </StyledLi>
+                </StyledUl>
+            </StyledMain>
         </>
     )
 }
