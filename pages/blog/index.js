c91e44205ff240633ee42ebc61b0653d8a680ebe
Joschka Eickhoff <jeickhoff@posteo.de>
2022-11-23 15:01:32 +0100

    added blog links

---
+++ pages/blog/index.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Projects() {
+    return (
+        <>
+        <Link href="/">go back</Link>
+            <p>This is the Blog page</p>
+        </>
+    )
+}
