c1c9b6a867f73d7a2efc3017ac39ee11ab5e60b1
Joschka Eickhoff <jeickhoff@posteo.de>
2022-11-23 22:06:26 +0100

    cleaned up

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Projects() {
     return (
         <>
-        <Link href="/">go back</Link>
+            <Link href="/">go back</Link>
             <p>This is the Blog page</p>
         </>
     )
