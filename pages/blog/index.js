066e3f24b09ee226ada16ced1c7885286eba5ed4
Joschka Eickhoff <jeickhoff@posteo.de>
2022-11-25 23:57:38 +0100

    first blog post

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -4,7 +4,13 @@ export default function Projects() {
     return (
         <>
             <Link href="/">go back</Link>
-            <p>This is the Blog page</p>
+            <h1>This is the Blog page</h1>
+            <section>
+                <article>
+                    <h2>Topic 1</h2>
+                    <p>Here stands something.</p>
+                </article>
+            </section>
         </>
     )
 }
