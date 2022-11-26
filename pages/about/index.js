deb7508891b367cf0fa5e0453cbd2e10e087c90d
Joschka Eickhoff <jeickhoff@posteo.de>
2022-11-26 17:21:25 +0100

    cleaning up

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,9 +1,10 @@
 import Link from "next/link";
+import PageHeader from "../../components/PageHeader";
 
 export default function About() {
     return (
         <>
-            <Link href="/">⬅️ go back</Link>
+            <PageHeader />
             <p>This is the about page</p>
         </>
     )
