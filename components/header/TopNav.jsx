1ef8f982ed1dbf527bcacc7a409ec45a21fb18e7
Joschka Eickhoff <jeickhoff@posteo.de>
2022-11-30 15:08:47 +0100

    ADDED TopNav.jsx

---
+++ components/header/TopNav.jsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import Link from "next/link";
+
+const StyledDiv = styled.div`
+  padding: 40px 50px 20px 50px;
+  text-align: right;
+  font-size: var(--fontsize_pageTopLinks);
+`;
+const StyledUl = styled.ul`
+  display: grid;
+  grid-template-columns: 1.5fr 1.5fr 2fr;
+  grid-template-rows: 100%;
+  padding-left: 30%;
+`;
+const StyledLi = styled.li`
+  list-style: none;
+  text-align: center;
+  /* border: 1px solid black; */
+  &:last-child {
+    text-align: right;
+  }
+`;
+
+console.clear();
+
+export default function TopNav({data}) {
+    
+  return (
+    <StyledDiv>
+      <nav>
+        <StyledUl>
+          {data.map(item => (
+              <StyledLi key={item.name}>
+                <Link href={item.href}>{item.name}</Link>
+              </StyledLi>
+              )
+          )}
+        </StyledUl>
+      </nav>
+    </StyledDiv>
+  )
+}
