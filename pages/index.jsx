f303ddcbee866b07c97812729dcf7a8622408fcc
Joschka Eickhoff <jeickhoff@posteo.de>
2022-12-08 15:14:03 +0100

    main page now responsive

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,37 +1,53 @@
 import Link from "next/link";
-import styled from 'styled-components';
+import styled from "styled-components";
+
+const StyledContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+  justify-content: center;
+  align-content: center;
+`;
 
 const StyledMain = styled.main`
-    width: 100vw;
-    height: 100vh;
-    display: grid;
-    justify-content: center;
-    align-content: center;
+  width: 100%;
+  height: 100%;
+  display: grid;
+  justify-content: center;
+  align-content: center;
+
+  @media screen and (min-width: 400px) {
+    width: 375px;
+    height: 667px;
+    border: 1px solid black;
+  }
 `;
 
 const StyledLi = styled.li`
-    font-size: var(--fontsize_profileButtons);
-    text-align: center;
-    list-style: none;
-    margin: 50px 0; 
+  font-size: var(--fontsize_profileButtons);
+  text-align: center;
+  list-style: none;
+  margin: 50px 0;
 `;
 
 export default function Home() {
-    return (
-        <>
-            <StyledMain>
-                <ul>
-                    <StyledLi>
-                        <Link href="/about">about</Link>
-                    </StyledLi>
-                    <StyledLi>
-                        <Link href="/projects">projects</Link>
-                    </StyledLi>
-                    <StyledLi>
-                        <Link href="/blog">blog</Link>
-                    </StyledLi>
-                </ul>
-            </StyledMain>
-        </>
-    )
+  return (
+    <>
+      <StyledContainer>
+        <StyledMain>
+          <ul>
+            <StyledLi>
+              <Link href="/about">about</Link>
+            </StyledLi>
+            <StyledLi>
+              <Link href="/projects">projects</Link>
+            </StyledLi>
+            <StyledLi>
+              <Link href="/blog">blog</Link>
+            </StyledLi>
+          </ul>
+        </StyledMain>
+      </StyledContainer>
+    </>
+  );
 }
