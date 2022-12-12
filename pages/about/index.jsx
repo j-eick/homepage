03c3e384d440d51cc1eb09fd422b205fa5e5a4fc
Joschka Eickhoff <jeickhoff@posteo.de>
2022-12-12 17:49:30 +0100

    need to make style changes in order to apply global layout

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -1,19 +1,45 @@
 import PageHeader from "../../components/header/PageHeader";
 import Main from "../../components/aboutPage/Main";
+import styled from "styled-components";
+
+// const StyledContainer = styled.div`
+//   width: 100vw;
+//   height: 100vh;
+//   display: grid;
+//   justify-content: center;
+//   align-content: center;
+// `;
+
+// const StyledMain = styled.main`
+//   width: 100%;
+//   height: 100%;
+//   display: grid;
+//   justify-content: center;
+//   align-content: center;
+
+//   @media screen and (min-width: 400px) {
+//     width: 375px;
+//     height: 667px;
+//     border: 1px solid black;
+//   }
+// `;
 
 export default function About() {
-
-    return (
-        <>
-            <PageHeader 
-                data={[
-                        {href:'/projects', name:'projects'},
-                        {href:'/blog', name:'blog'},
-                        {href:'/', name: 'home'}]
-                    }
-                title={"Hi!"}
-            />
-            <Main />
-        </>
-    )
+  return (
+    <>
+      {/* <StyledContainer>
+        <StyledMain> */}
+      <PageHeader
+        data={[
+          { href: "/projects", name: "projects" },
+          { href: "/blog", name: "blog" },
+          { href: "/", name: "home" },
+        ]}
+        title={"Hi!"}
+      />
+      <Main />
+      {/* </StyledMain>
+      </StyledContainer> */}
+    </>
+  );
 }
