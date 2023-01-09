e538437a6f70be2fde208fa9d1326ef763c12720
Joschka Eickhoff <jeickhoff@posteo.de>
2023-01-09 10:44:13 +0100

    minor details

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -1,9 +1,10 @@
 import Main from "../../components/blogPage/Main";
 import TopNav from "../../components/header/TopNav";
+import Layout from "../../components/SubpageLayout";
 
 export default function Blog() {
   return (
-    <>
+    <Layout>
       <TopNav
         data={[
           { href: "/about", name: "about" },
@@ -12,6 +13,6 @@ export default function Blog() {
         ]}
       />
       <Main />
-    </>
+    </Layout>
   );
 }
