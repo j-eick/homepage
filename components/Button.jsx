255636c0cb634f4199d8df87bef45df88bb52f41
Joschka Eickhoff <jeickhoff@posteo.de>
2023-01-19 16:30:37 +0100

    created button component

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,9 +1,31 @@
 import styled from "styled-components";
 
-const StyledButton = styled.button``;
+const StyledButton = styled.button`
+  text-decoration: none;
+  border: 2px solid #764abc;
+  border-radius: 25px;
+  color: #764abc;
+  padding: 10px 20px;
+  transition: all 1s;
+  position: relative;
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #764abc;
+    transition: all 1s;
+  }
+`;
 
 const Button = () => {
-  return <Button></Button>;
+  return <StyledButton>Bla</StyledButton>;
 };
 
-export default Example;
+export default Button;
+
+/**
+
+ */
