2558201e687381ee56c081ebe5f12e0950bfce78
Joschka Eickhoff <jeickhoff@posteo.de>
2023-01-20 11:50:26 +0100

    created ProjectTile-component

--- components/projectPage/Main.jsx
+++ components/projectPage/Main.jsx
@@ -1,24 +1,17 @@
 import Link from "next/link";
 import styled from "styled-components";
 import { StyledUl, StyledLi, StyledP } from "./Main.styles.js";
+import ProjectTile from "./ProjectTile.jsx";
 
 console.clear();
 
 export default function Main() {
   return (
     <StyledUl>
-      <StyledLi>
-        <StyledP>something</StyledP>
-      </StyledLi>
-      <StyledLi>
-        <StyledP>something</StyledP>
-      </StyledLi>
-      <StyledLi>
-        <StyledP>something</StyledP>
-      </StyledLi>
-      <StyledLi>
-        <StyledP>something</StyledP>
-      </StyledLi>
+      <ProjectTile />
+      <ProjectTile />
+      <ProjectTile />
+      <ProjectTile />
     </StyledUl>
   );
 }
---
+++ components/projectPage/ProjectTile.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { StyledLi, StyledP } from "./Main.styles";
+
+export default function ProjectTile() {
+  return (
+    <StyledLi>
+      <StyledP>something</StyledP>
+    </StyledLi>
+  );
+}
