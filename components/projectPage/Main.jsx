d35404ee8f6330e9e93ec1674d3249043b86a31e
Joschka Eickhoff <jeickhoff@posteo.de>
2023-01-20 11:29:21 +0100

    separated ProjectPage styles

--- components/projectPage/Main.jsx
+++ components/projectPage/Main.jsx
@@ -1,28 +1,44 @@
 import Link from "next/link";
 import styled from "styled-components";
+import { StyledUl, StyledLi, StyledP } from "./Main.styles.js";
 
-const StyledUl = styled.ul`
-  display: grid;
-  grid-template-columns: 1fr;
-  gap: 30px;
-  padding: 0px 50px;
-`;
-const StyledLi = styled.li`
-  display: grid;
-  place-items: center;
+console.clear();
 
-  height: 80px;
-  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
-  /* box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px; */
-  border-radius: 20px;
-  &:after:hover {
-    transition: box-shadow 0.3s;
-    box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
-  }
-`;
-const StyledP = styled.p`
-  font-size: var(--fontsize_pageText);
-`;
+// const StyledUl = styled.ul`
+//   display: grid;
+//   grid-template-columns: 1fr;
+//   gap: 30px;
+//   padding: 0px 50px;
+// `;
+// const StyledLi = styled.li`
+//   display: grid;
+//   place-items: center;
+
+//   height: 80px;
+//   position: relative;
+//   box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
+//   border-radius: 20px;
+//   transition: all 0.1s ease-in-out;
+//   &:hover {
+//     transform: scale(1.1);
+//   }
+//   &:hover::after {
+//     opacity: 1;
+//   }
+//   &::after {
+//     content: "";
+//     position: absolute;
+//     width: 100%;
+//     height: 100%;
+//     box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
+//     border-radius: 20px;
+//     opacity: 0;
+//     transition: opacity 0.1s ease-in-out;
+//   }
+// `;
+// const StyledP = styled.p`
+//   font-size: var(--fontsize_pageText);
+// `;
 
 export default function Main() {
   return (
