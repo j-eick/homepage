9273e8a64eb990bbe72ad6d23291bda28551e713
Joschka Eickhoff <jeickhoff@posteo.de>
2023-01-31 12:47:28 +0100

    style fixes

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -3,11 +3,9 @@ import Canvas from "../styles/MainLayout.module.css";
 export default function Layout({ children }) {
   return (
     <>
-      {/* <div className={Canvas.background}> */}
       <div className={Canvas.canvas}>
         <div className={Canvas.responsiveCanvas}>{children}</div>
       </div>
-      {/* </div> */}
     </>
   );
 }
