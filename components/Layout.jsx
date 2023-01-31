dc718d843df55a577e1243abf3e6590ac15b497f
Joschka Eickhoff <jeickhoff@posteo.de>
2023-01-31 12:35:41 +0100

    basic blurry background; needs refinement

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -3,11 +3,11 @@ import Canvas from "../styles/MainLayout.module.css";
 export default function Layout({ children }) {
   return (
     <>
-      <div className={Canvas.background}>
-        <div className={Canvas.canvas}>
-          <div className={Canvas.responsiveCanvas}>{children}</div>
-        </div>
+      {/* <div className={Canvas.background}> */}
+      <div className={Canvas.canvas}>
+        <div className={Canvas.responsiveCanvas}>{children}</div>
       </div>
+      {/* </div> */}
     </>
   );
 }
