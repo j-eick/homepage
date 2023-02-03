c565df6b0b0668d84b8e5b91b7d0287340a9a2a4
Joschka Eickhoff <jeickhoff@posteo.de>
2023-02-03 16:24:15 +0100

    created run-graph projectPage

---
+++ pages/projects/run-graph.jsx
@@ -0,0 +1,10 @@
+import Layout from "../../components/Layout-Subpage";
+import PageHeader from "../../components/header/PageHeader";
+
+export default function RunGraph() {
+  return (
+    <Layout>
+      <PageHeader data={[{ href: "/projects", name: "about" }]} title={"A little overview..."} />
+    </Layout>
+  );
+}
