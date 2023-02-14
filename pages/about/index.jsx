23748990e488e2de3c6f9e4d605c181c69601d53
Joschka Eickhoff <jeickhoff@posteo.de>
2023-02-14 13:46:40 +0100

    DELETED aboutPage Component

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -1,6 +1,13 @@
 import PageHeader from "../../components/header/PageHeader";
-import Main from "../../components/aboutPage/Main";
 import Layout from "../../components/Layout-Subpage";
+import Link from "next/link";
+import styled from "styled-components";
+
+const StyledP = styled.p`
+  padding: 20px 50px;
+  text-align: center;
+  font-size: var(--fontsize_pageText);
+`;
 
 export default function About() {
   return (
@@ -13,7 +20,19 @@ export default function About() {
         ]}
         title={"Hi!"}
       />
-      <Main />
+      <StyledP>
+        My name is Josh and I craft applications for the web.
+        <br />
+        <br />
+        I regard coding as an art, which embodies the door to endless creativity and possibilities.
+        <br />
+        <br />
+        <br />
+        My <Link href="projects">projects page</Link> gives you an overview over my recent workings.
+        <br />
+        My <Link href="/blog">blog page</Link> addresses topics that are meaningful to me. This is a chance to get to
+        know me a little better.
+      </StyledP>
     </Layout>
   );
 }
