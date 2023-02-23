d1c44a8d5e46bcae907d0e4f8bfbead20a5ed02e
Joschka Eickhoff <jeickhoff@posteo.de>
2023-02-23 15:55:12 +0100

    fixing things

--- pages/projects/run-graph/index.jsx
+++ pages/projects/run-graph/index.jsx
@@ -1,11 +1,11 @@
-import Layout_Subpage from "../../styles/SubpageLayout";
+// import Layout_Subpage from "../../../styles/SubpageLayout.module.css";
+import Layout_Subpage from "../../../components/Layout-Subpage";
 import Header from "../../../components/header/Header";
 
 export default function RunGraph() {
   return (
     <Layout_Subpage>
       <Header data={[{ href: "/projects", name: "back" }]} />
-      <p>stuff</p>
     </Layout_Subpage>
   );
 }
