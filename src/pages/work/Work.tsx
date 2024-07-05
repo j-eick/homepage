295414cefa9f995e0c4a4491d4de0df102a4c651
Joschka Eickhoff <jeickhoff@posteo.de>
2024-07-05 17:31:15 +0200

    new / Work page

---
+++ src/pages/work/Work.tsx
@@ -0,0 +1,10 @@
+import Nav from "../../components/ui/nav/Nav";
+import styles from "./work.module.css";
+
+export default function Work() {
+  return (
+    <main>
+      <Nav />
+    </main>
+  );
+}
